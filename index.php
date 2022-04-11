429caf0053aeb11a9cb6633e9722fc7e660f21cb
Carlos J Ramirez <carlosjramirez11@gmail.com>
2022-04-10 17:53:43 -0700

    Switched from using pos params to named params

--- index.php
+++ index.php
@@ -13,8 +13,41 @@ $dsn = "mysql:host=$host;dbname=$dbname";
 $pdo = new PDO($dsn, $user, $password);
 
 //PDO Query
-$stmt = $pdo->query('SELECT * FROM post');
+// $stmt = $pdo->query('SELECT * FROM post');
 
-while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-    echo $row["title"] . "<br />";
+//Echo result as array
+// while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+//     echo $row["title"] . "<br />";
+// }
+
+//Echo result as object
+// while ($row = $stmt->fetch(PDO::FETCH_OBJ)) {
+//     echo $row->title . "<br />";
+// }
+
+//Prepared Statements
+
+//Fetch Mutile Posts
+
+//User Input
+$author = "CJ";
+$is_published = true;
+
+//Positional Params
+// $sql = "SELECT * FROM post WHERE author = ?";
+// $stmt = $pdo->prepare($sql);
+// $stmt->execute([$author]);
+// $posts = $stmt->fetchAll(PDO::FETCH_OBJ);
+
+//Named Params
+$sql = "SELECT * FROM post WHERE author = :author && is_published = :is_published";
+$stmt = $pdo->prepare($sql);
+$stmt->execute(["author" => $author, "is_published" => $is_published]);
+$posts = $stmt->fetchAll(PDO::FETCH_OBJ);
+
+// var_dump($posts);
+
+//Echos the title of every post returned
+foreach ($posts as $post) {
+    echo $post->title . "<br />";
 }
