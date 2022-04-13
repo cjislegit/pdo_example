e85a7738ab87f3e58faf8d7f6a056b2aa9763b11
Carlos J Ramirez <carlosjramirez11@gmail.com>
2022-04-12 20:36:01 -0700

    Finished creating the PDO reference

--- index.php
+++ index.php
@@ -12,6 +12,9 @@ $dsn = "mysql:host=$host;dbname=$dbname";
 //Create PDO instance
 $pdo = new PDO($dsn, $user, $password);
 
+//Set the default return data to an object
+$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+
 //PDO Query
 // $stmt = $pdo->query('SELECT * FROM post');
 
@@ -68,11 +71,50 @@ $id = 1;
 // echo $postCount;
 
 //Insert Data
-$title = "Post Five";
-$body = "This is the fifth post.";
-$author = "Carlos";
+// $title = "Post Five";
+// $body = "This is the fifth post.";
+// $author = "Carlos";
 
-$sql = "INSERT INTO post(title, body, author) VALUES(:title, :body, :author)";
+// $sql = "INSERT INTO post(title, body, author) VALUES(:title, :body, :author)";
+// $stmt = $pdo->prepare($sql);
+// $stmt->execute(["title" => $title, "body" => $body, "author" => $author]);
+// echo "Post Added";
+
+//Update Data
+// $id = 1;
+// $body = "Update to post 1";
+
+// $sql = "UPDATE post SET body = :body WHERE id = :id";
+// $stmt = $pdo->prepare($sql);
+// $stmt->execute(["body" => $body, "id" => $id]);
+// echo "Post Updated";
+
+//Delete Data
+// $id = 3;
+
+// $sql = "DELETE FROM post WHERE id = :id";
+// $stmt = $pdo->prepare($sql);
+// $stmt->execute(["id" => $id]);
+// echo "Post Deleted";
+
+//SEARCH Data
+// $search = "%Five%";
+// $sql = "SELECT * FROM post WHERE title LIKE :search";
+// $stmt = $pdo->prepare($sql);
+// $stmt->execute(["search" => $search]);
+// $posts = $stmt->fetchAll(PDO::FETCH_OBJ);
+
+// foreach ($posts as $post) {
+//     echo $post->title . "<br />";
+// }
+
+//Limit Posts
+
+$sql = "SELECT * FROM post WHERE author = :author && is_published = :is_published LIMIT 1";
 $stmt = $pdo->prepare($sql);
-$stmt->execute(["title" => $title, "body" => $body, "author" => $author]);
-echo "Post Added";
+$stmt->execute(["author" => $author, "is_published" => $is_published]);
+$posts = $stmt->fetchAll(PDO::FETCH_OBJ);
+
+foreach ($posts as $post) {
+    echo $post->title . "<br />";
+}
